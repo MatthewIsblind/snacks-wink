6cafa22f1fc9f6be19e22e6c943ed948f86851e1
MatthewIsBlind <matet2501@gmail.com>
2023-10-01 14:39:37 +1000

    start of the project

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace snacks_wink
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
